fe03a3bfdc0b06230fb8a46833e540e3b0aac859
GUAN JunYi <314216363@qq.com>
2021-04-18 16:22:41 +0800

    Update test_dataClustering.m

--- test_dataClustering.m
+++ test_dataClustering.m
@@ -1,6 +1,6 @@
 % Please kindly cite the paper Junyi Guan, Sheng Li, Xiongxiong He, and Jiajia Chen, 
 %"Peak-graph-based fast density peak clustering for image segmentation," 
-% IEEE SIGNAL PROCESSING LETTERS, 2021,Doi:?
+% IEEE SIGNAL PROCESSING LETTERS, 2021,Doi:10.1109/LSP.2021.3072794
 % The code was written by Junyi Guan in 2020.
 clear all; close all; clc;
 %% loading data
